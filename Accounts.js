17f00975ba0a3e3d6a5353bcf16d8f5bd053b8db
Pyronicxx <148806970+Pyronicxx@users.noreply.github.com>
2025-07-01 11:41:01 +0200

    color

--- Accounts.js
+++ Accounts.js
@@ -10,3 +10,26 @@ let setBalance = balance => {
     cookies.account = account;
     document.querySelector("#balance").innerText = `BTC ${account.balance}`;
 };
+
+ const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const user = document.getElementById("username").value;
+        const pass = document.getElementById("password").value;
+        const errorMsg = document.getElementById("error-msg");
+
+        if (user === "Max" && pass === "Praktikum") {
+            errorMsg.style.color = "green";
+            errorMsg.textContent = "Login erfolgreich!, Max";
+
+            setTimeout(() => {
+                window.location.href = "Homepage.html";
+            }, 1000);
+        } else {
+            errorMsg.style.color = "red";
+            errorMsg.textContent = "Benutzername oder Passwort falsch.";
+        }
+    });
+}
