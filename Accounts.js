bb4c2c8a609470dd01e2e9a278dd937c5479860f
thefrostypixel <thefrostypixel@gmail.com>
2025-07-02 12:39:02 +0200

    Password Hash, Slots

--- Accounts.js
+++ Accounts.js
@@ -1,8 +1,13 @@
 let accounts = cookies.accounts ?? [
     {
-        name: "Max",
-        password: "Phonecase",
+        name: "MaxH",
+        passwordHash: 4427849782227161,
         balance: 100,
+    },
+    {
+        name: "MaxB",
+        passwordHash: 4721474318246072,
+        balance: 10000,
     },
 ];
 
@@ -10,6 +15,22 @@ let visitorAccount = {
     name: "Visitor",
     balance: 100,
 };
+
+let hashStr = (str, seed = 0) => {
+    let h1 = 0xAD601D07 ^ seed;
+    let h2 = 0x41C6CE57 ^ seed;
+    [...str].forEach(char => {
+        h1 = Math.imul(h1 ^ char.charCodeAt(0), 0x9E3779B1);
+        h2 = Math.imul(h2 ^ char.charCodeAt(0), 0x5F356495);
+    });
+    h1 = Math.imul(h1 ^ h1 >>> 16, 0x85EBCA6B);
+    h2 = Math.imul(h2 ^ h2 >>> 16, 0x85EBCA6B);
+    h1 = (h1 ^ Math.imul(h2 ^ h2 >>> 13, 0xC2B2AE35)) >>> 0;
+    h2 = (h2 ^ Math.imul(h1 ^ h1 >>> 13, 0xC2B2AE35)) >>> 0;
+    return h1 + (h2 & 0x1FFFFF) * 0x100000000;
+};
+
+let hashPassword = (name = "Visitor", password = "", project = "website-DB") => hashStr(name, hashStr(password, hashStr(project)));
 
 let account = accounts.find(account => account.name == cookies.account) ?? visitorAccount;
 let updateAccount = () => {
--- slots.js
+++ slots.js
@@ -3,8 +3,8 @@ let slotsSymbols = {
     Lemon: [8, 15],
     Plum: [6, 10],
     Bell: [4, 50],
-    Diamond: [2, 100],
-    Lucky7: [1, 150],
+    Clover: [2, 100],
+    Coin: [1, 150],
 };
 
 let pickSlotSymbol = (advantage = 0) => {
@@ -17,32 +17,56 @@ let pickSlotSymbol = (advantage = 0) => {
     }
 };
 
-let spinSlots = (t = 200, s = .2, advantage = 0) => {
-    let rolls = [];
+let spinSlots = (advantage = 0, t = 200, s = .35) => {
+    let tMults = [.4, .55, .7, .85, 1];
+    let symbols = [];
     let html = "";
-    let v = 0;
-    for (let j = 0; j < t; j++) {
-        v -= s * (1 - (j / t) ** 2);
-    }
+    let vs = [];
     for (let i = 0; i < 5; i++) {
-        let roll = [];
-        for (let j = 0; j < Math.ceil(v); j++) {
-            let symbol = pickSlotSymbol(advantage);
-            roll.push(symbol);
+        let symbolColumn = [];
+        html += "<span>";
+        vs.push(0);
+        for (let j = 0; j < t * tMults[i]; j++) {
+            vs[i] -= s * (1 - (j / t / tMults[i]) ** 2);
         }
-        rolls.push(roll);
-        html += `<span>${`<img src=''>`.repeat(4)}</span>`;
+        for (let j = 0; j < 3; j++) {
+            let symbol = pickSlotSymbol(advantage);
+            html += `<img src="imgs/${pickSlotSymbol(advantage)}.png">`;
+            symbolColumn.push(symbol);
+        }
+        for (let j = 0; j < Math.ceil(-vs[i]); j++) {
+            html += `<img src="imgs/${pickSlotSymbol(advantage)}.png">`;
+        }
+        symbols.push(symbolColumn);
+        html += "</span>";
     }
     document.querySelector("#slots-rolls").innerHTML = html;
-    for (let i = 0; i < t; i++) {
-        setTimeout(() => {
-            v += s * (1 - (i / t) ** 2);
-            [...document.querySelectorAll("#slots-rolls > *")].forEach((roll, j) => {
-                roll.style.transform = `translateY(${(v % 1) * 100}px)`;
-                [...roll.children].forEach((slot, k) => {
-                    slot.src = `imgs/${rolls[j][k]}.png`;
-                });
-            });
-        }, i * 20);
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < t * tMults[i]; j++) {
+            setTimeout(() => {
+                vs[i] += s * (1 - (j / t / tMults[i]) ** 2);
+                document.querySelector("#slots-rolls").children[i].style.transform = `translateY(${vs[i] * 100}px)`;
+            }, j * 20);
+        }
     }
+    setTimeout(() => {
+        let score = 0;
+        for (let x = 0; x < 5; x++) {
+            if (symbols[x][0] == symbols[x][1] && symbols[x][1] == symbols[x][2]) {
+                console.log(symbols[x][0]);
+                score += slotsSymbols[symbols[x][0]][1];
+            }
+        }
+        for (let y = 0; y < 3; y++) {
+            if (symbols[0][y] == symbols[1][y] && symbols[1][y] == symbols[2][y] && symbols[2][y] == symbols[3][y] && symbols[3][y] == symbols[4][y]) {
+                score += slotsSymbols[symbols[2][y]][1] * 5;
+            } else if (symbols[1][y] == symbols[2][y] && symbols[2][y] == symbols[3][y] && (symbols[0][y] == symbols[1][y] || symbols[3][y] == symbols[4][y])) {
+                score += slotsSymbols[symbols[2][y]][1] * 3;
+            } else if (symbols[1][y] == symbols[2][y] && (symbols[0][y] == symbols[1][y] || symbols[2][y] == symbols[3][y]) || symbols[2][y] == symbols[3][y] || symbols[3][y] == symbols[4][y]) {
+                score += slotsSymbols[symbols[2][y]][1];
+            }
+        }
+        console.log(symbols);
+        console.log(score);
+    }, t * 20);
 };
